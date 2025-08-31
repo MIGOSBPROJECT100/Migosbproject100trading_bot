135fdf0eb37e865563a20c329720104e58d8006d
Migos B.™ <ndungukibera01@gmail.com>
2025-08-31 09:42:53 +0300

    Delete ui directory

--- ui/__init__.py
+++
@@ -1,8 +0,0 @@
-# ui package initializer
-from .menus import main_menu, signals_menu, account_menu, premium_menu, news_menu, admin_menu
-from .texts import WELCOME, TERMS, DISCLAIMER, LOCKDOWN_NOTICE
-
-__all__ = [
-    "main_menu", "signals_menu", "account_menu", "premium_menu", "news_menu", "admin_menu",
-    "WELCOME", "TERMS", "DISCLAIMER", "LOCKDOWN_NOTICE"
-]
